e7c169602ae579d2bb85f1b0ce0d8d97a5621ac6
Siddhi Khalate <93478859+siddhikhalate@users.noreply.github.com>
2024-09-30 06:17:11 +0000

    data type summary

---
+++ Basicjavascript/datatypesSummary.js
@@ -0,0 +1,39 @@
+// #primitive 
+
+// 7 types : String, Number,Boolearn, null,undefined,Symbol,BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 34556677667887532
+
+
+
+// Reference(Non primitive)
+
+// Array , objects , Functions
+
+const heros = ["shaktiman","naagraj","doga"];
+let myObj={
+    name:"siddhi",
+    age:23,
+
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+console.log(typeof bigNumber);
+console.log(typeof outsideTemp);console.log(typeof scoreValue );
+
+console.log(typeof myFunction);
+
