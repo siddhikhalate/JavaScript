8a643d92c93ba4c85769d921f34b20c89d65bf69
Siddhi Khalate <93478859+siddhikhalate@users.noreply.github.com>
2024-09-30 05:37:47 +0000

    operators

--- Basicjavascript/conversionoperators.js
+++ Basicjavascript/conversionoperators.js
@@ -2,12 +2,12 @@ let score = "33abc"
 
 //null given output 0
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" =>33
 // "33abc" =>NaN
@@ -25,6 +25,56 @@ let booleaIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 77
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ***************Operation********************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " siddhi"
+
+let str3 = str1 +str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" +2 + 2);
+// console.log(1 +2 + "2");
+
+// console.log(3 + 4 * 5 % 3);
+
+// console.log(true) // (+true) given output 1
+
+let num1, num2,num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+
+
+
+
+
+
+
+
+
+
 
