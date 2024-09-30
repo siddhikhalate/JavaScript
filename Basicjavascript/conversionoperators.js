6329901558f0795d927119890c65f87d79874a42
Siddhi Khalate <93478859+siddhikhalate@users.noreply.github.com>
2024-09-30 05:12:49 +0000

    conversion of data types

---
+++ Basicjavascript/conversionoperators.js
@@ -0,0 +1,30 @@
+let score = "33abc" 
+
+//null given output 0
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" =>33
+// "33abc" =>NaN
+// true =>1; false =>0
+
+let isLoggedIn = "siddhi"
+
+let booleaIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleaIsLoggedIn);
+
+// 1 => true; 0 =>false
+// "" =>false
+// "hitesh" =>true
+
+let someNumber = 77
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
