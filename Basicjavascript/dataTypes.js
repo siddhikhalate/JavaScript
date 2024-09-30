d759c2b6dd7913d49379eed686fe31f814a24145
Siddhi Khalate <93478859+siddhikhalate@users.noreply.github.com>
2024-09-30 04:51:02 +0000

    datatypes

---
+++ Basicjavascript/dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all JS code as newer version
+// alert(3+3) we are using node.js, not browser
+
+console.log("Siddhi");
+
+let name = "siddhi"
+let age =23
+let isLoggedIn = false
+let state
+
+// number =>2 to power 53
+// bigint => use in big number
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol=>find unique ness 
+
+
+// object
+
+console.log(typeof "siddhi");
+console.log(typeof age);
+console.log(typeof null);//object
+console.log(typeof undefined);  //undefined 
+
+
+
+
+
