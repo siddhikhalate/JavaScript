ca3326f8192742f45bfcad14f3c9e1e330321e96
Siddhi Khalate <93478859+siddhikhalate@users.noreply.github.com>
2024-10-03 15:57:34 +0000

    truthy_falsy

--- Control_Flow/truthy.js
+++ Control_Flow/truthy.js
@@ -20,5 +20,23 @@ if(userEmail){
 const emptyObj = {}
 if(Object.keys(emptyObj)){
     console.log("Object is empty");
-    
 }
+
+// Nullish Coalescing operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1= null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20
+
+console.log(val1);
+
+
+// Terniary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") :console.log("More than 80");
+
